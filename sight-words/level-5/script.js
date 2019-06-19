f7bd0bd3ec9bde58ad1685726baa60529136d29b
Bets <elizabeth.lidstrom@gmail.com>
2019-06-19 15:46:16 -0400

    level 5 added, palette update

---
+++ sight-words/level-5/script.js
@@ -0,0 +1,57 @@
+
+setTimeout( function(){
+	document.getElementById('sneaky').style.display = 'block';}, 4000 );
+
+var els = document.querySelectorAll('#hexagon'),
+	words = ['pool', 'splash', 'dive', 'towel',
+	'float', 'sunny', 'goggles', 'ball', 'noodle',
+	'deep'],
+	ms = 2500,
+	interval;
+
+function start(els, words, ms){
+	var i = 0;
+	interval = setInterval(function(){
+		[].forEach.call(els, function(el){
+			el.innerHTML = words[i];
+		})
+		i += 1;
+		i === words.length ? i = 0 : false;
+	}, ms);
+}
+
+function stop(){
+	clearInterval(interval);
+}
+
+var x = document.getElementById('start');
+var y = document.getElementById('stop');
+
+x.addEventListener('click', function(){
+	start(els, words, ms);
+	x.classList.add('green-glow');
+	setTimeout(() => x.classList.remove('green-glow'), 3000);
+});
+
+y.addEventListener('click', function(){
+	stop();
+	y.classList.add('red-glow');
+	setTimeout(() => y.classList.remove('red-glow'), 3000);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
